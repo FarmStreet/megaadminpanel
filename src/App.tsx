5bbe8ecd041bc1ae25e67a4e71a24520d44378a8
alexaos <alex2000mn@gmail.com>
2022-10-06 11:23:13 +0500

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
 
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>переключить</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
