fca353fb4b4a64e5a0fa08c74c5cfce2ba63ff5e
alexaos <alex2000mn@gmail.com>
2022-10-10 15:09:25 +0500

    add preloader

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,3 +1,7 @@
 import { lazy } from 'react';
 
-export const MainPageAsync = lazy(() => import('./MainPage'));
+export const MainPageAsync = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    // just for tests
+    setTimeout(() => resolve(import('./MainPage')), 2000);
+}));
