687707f62fa11ea463e8c21e9e5c33dad1cb25c9
alexaos <alex2000mn@gmail.com>
2022-10-17 13:54:28 +0500

    some optimization for prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,6 +6,7 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+
     return [
         new HTMLWebpackPlugin({
             template: paths.html,
@@ -18,8 +19,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
+        isDev && new webpack.HotModuleReplacementPlugin(),
+        isDev && new BundleAnalyzerPlugin({
             openAnalyzer: false,
         }),
         isDev && new ReactRefreshWebpackPlugin(),
